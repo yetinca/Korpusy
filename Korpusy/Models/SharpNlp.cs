b0dae9a75be30ab49815d41f0e12eefec7bac254
yetinca <sdziew@gmail.com>
2017-04-14 15:18:25 +0200

    korpu

--- Korpusy/Models/SharpNlp.cs
+++ Korpusy/Models/SharpNlp.cs
@@ -1,16 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using edu.stanford.nlp.ling;
+using edu.stanford.nlp.tagger.maxent;
+using java.io;
+using java.util;
+using Console = System.Console;
 
 namespace Korpusy.Models
 {
   public class SharpNlp
+  
 
-  {
-    public string mModelPath = @"C:/Users/Seba/Documents/Visual Studio 2017/Projects/Korpusy/Korpusy/Models/EnglishPOS.nbin";
-
-    public mPosTagger = new OpenNLP.Tools.PosTagger.EnglishMaximumEntropyPosTagger(mModelPath + "EnglishPOS.nbin", mModelPath + @"/Parser/tagdict");
+  
+    {
+      public void Main()
+      {
+        var jarRoot = @"G:\ASP\stanford-postagger-full-2016-10-31";
+        var modelsDirectory = jarRoot + @"\models";
+        // Loading POS Tagger
+        var tagger = new MaxentTagger(modelsDirectory + @"\wsj-0-18-bidirectional-nodistsim.tagger");
+      // Text for tagging
+      var text = System.IO.File.ReadAllText(@"C:\Users\Seba\Documents\!Studia\Korpus.txt");
+        var sentences = MaxentTagger.tokenizeText(new StringReader(text)).toArray();
+        foreach (ArrayList sentence in sentences)
+        {
+          var taggedSentence = tagger. tagSentence(sentence);
+        //  Console.WriteLine(SentenceUtils.listToString(taggedSentence, false));
+        //Console.ReadLine();
+        }
+      }
+    }
   }
-}
+
+
--- Korpusy/Program.cs
+++ Korpusy/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.IO;
-using System.Linq;
-using edu.stanford.nlp.ling;
-using edu.stanford.nlp.tagger.maxent;
-
+﻿using Korpusy.Models;
 
 namespace Korpusy
 {
@@ -18,25 +12,28 @@ namespace Korpusy
 
             var bazunia = new KorpusyEntities();
 
+      var pos = new SharpNlp();
+      pos.Main();
+
       //bazunia.Database.ExecuteSqlCommand("delete from t1");
 
-     
+
       //(new StringReader(File.ReadAllText(@"C:\Users\Seba\Documents\!Studia\Korpus.txt")))
-   
-          //  foreach (var i in plik.Wyrazy(System.IO.File.ReadAllText(@"C:\Users\Seba\Documents\!Studia\Korpus.txt")))
-          //  {
-          //      if (i.Length < 22 && i.Length > 0 && !i.All(char.IsDigit))
 
-          //      {
-          //          bazunia.Database.ExecuteSqlCommand("insert into t1(word, lenght) values('" + i.Trim() + "'," +
-          //                                             i.Length + ")");
-          //      }
-          //      else
-          //      {
-          //System.Console.WriteLine(i);
-          //      }
-          //  }
+      //  foreach (var i in plik.Wyrazy(System.IO.File.ReadAllText(@"C:\Users\Seba\Documents\!Studia\Korpus.txt")))
+      //  {
+      //      if (i.Length < 22 && i.Length > 0 && !i.All(char.IsDigit))
 
-        }
+      //      {
+      //          bazunia.Database.ExecuteSqlCommand("insert into t1(word, lenght) values('" + i.Trim() + "'," +
+      //                                             i.Length + ")");
+      //      }
+      //      else
+      //      {
+      //System.Console.WriteLine(i);
+      //      }
+      //  }
+
+    }
     }
 }
